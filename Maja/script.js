632589adf36dce6f58c1e2a32c0814e12516d762
Maja <majamarek.design@gmail.com>
2020-09-21 14:01:06 +0200

    39 done

---
+++ Maja/script.js
@@ -0,0 +1,20 @@
+/* 1. 
+Lowest to Highest
+Given an array of numbers from 1-9: */
+
+let arr = [4, 2, 5, 3, 9, 7, 1, 8, 2, 6];
+
+numArray = arr.sort();
+console.log(numArray)  // [
+//  1, 2, 2, 3, 4,
+//  5, 6, 7, 8, 9
+//]
+
+/* 2. 
+Alphabetical Order
+Given an array of names: 
+Sort the array based on alphabetical order from a → z*/
+
+let arr2 = ["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"];
+stringArr = arr2.sort();
+console.log(stringArr);  // [ 'Ale', 'Bleda', 'Fran', 'Hadi', 'Jenna', 'Leon' ]
